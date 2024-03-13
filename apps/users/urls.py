3f272b0d7ff7adfe93ceea65ba19ac6904a3877f
Flavio Lopes De Souza <flsouza@cpqd.com.br>
2024-03-13 12:19:13 -0300

    alterações no arquivo static e no diretorio usado em base.html para styles, mudando de '/style/...' para 'style/...'

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('confirmacao/', views.confirmacao, name='confirmacao'),
+    path('', RedirectView.as_view(url='/confirmacao/', permanent=True)),
 ]
