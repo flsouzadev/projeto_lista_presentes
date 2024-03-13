d2d52fa24d9737f909f248859bfb1d0a3e71b71e
Flavio Lopes De Souza <flsouza@cpqd.com.br>
2024-03-13 04:52:09 -0300

    atualizando arquivo vercel.json

--- setup/settings.py
+++ setup/settings.py
@@ -17,7 +17,8 @@ from django.contrib.messages import constants as messages
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,13 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# URL base para arquivos estáticos
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/')
-]
+# Diretório onde os arquivos estáticos coletados serão armazenados durante o build
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Lista de diretórios onde o collectstatic procurará arquivos estáticos para coletar
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
